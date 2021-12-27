6895766fbc7bc16d3e87771a4a080ce6a4e736a3
ejimenez123 <caribestic123@gmail.com>
2021-12-27 18:47:38 -0500

    feat: Lesson 01 - Getting Started with Option API

---
+++ 01-v3-getting-started/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      message: "Hello world"
+    }
+  },
+  mounted() {
+    this.increment();
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+      console.log(this.$store.state.count)
+    }
+  }
+};
+
+</script> 
+
